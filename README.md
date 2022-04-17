85d1826abbf58edb274ab6b7af1e5dc8b7ac09af
Sudip Roy <sudiproy20yo@gmail.com>
2022-04-17 08:24:16 +0530

    Updated few things and published in hex

--- README.md
+++ README.md
@@ -11,17 +11,55 @@
 
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `identicon` to your list of dependencies in `mix.exs`:
+[available in Hex](https://hex.pm/packages/identicon2), the package can be installed
+by adding `identicon2` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
   [
-    {:identicon, "~> 0.1.0"}
+    {:identicon2, "~> 0.1.0"}
   ]
 end
 ```
 
+## Notes
+
+### EGD - Erlang Graphical Drawer
+
+`:egd` is no longer available in Elixir OTP, so to get around this
+
+mix.exs:
+
+```elixir
+{:egd, github: "erlang/egd"}
+```
+
+To install dependencies:
+
+```cli
+mix deps.clean --all
+mix deps.get
+mix deps.compile
+```
+
+### Generate docs
+
+mix.exs:
+
+```elixir
+{:ex_doc, "~> 0.12"}
+```
+
+```cli
+mix docs
+```
+
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at <https://hexdocs.pm/identicon>.
+be found at <https://hexdocs.pm/identicon2>.
+
+## License & copyright
+
+Copyright © 2022 Sudip Roy, All Rights Reserved
+
+Licensed under the [MIT License](/LICENSE)
--- mix.exs
+++ mix.exs
@@ -3,11 +3,15 @@ defmodule Identicon.MixProject do
 
   def project do
     [
-      app: :identicon,
+      app: :identicon2,
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Identicon2",
+      source_url: "https://github.com/RamzziSudip/Identicon/"
     ]
   end
 
@@ -21,9 +25,25 @@ defmodule Identicon.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:egd, github: "erlang/egd"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+      # {:egd, github: "erlang/egd"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    "This package converts any hash value, IP address, name into and unique image. The unique image will be the same for the same string"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "identicon2",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT License"],
+      links: %{"GitHub" => "https://github.com/RamzziSudip/Identicon"}
     ]
   end
 end
